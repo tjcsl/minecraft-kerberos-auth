0773957759da881971b55fda763d9cfb207a19f0
Samuel Damashek <2017sdamashe@minecraft.csl.tjhsst.edu>
2013-10-12 11:26:38 -0400

    Start on app.py, add index.html

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, request, render_template, send_file
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
-
+    if request.method == 'GET':
+        return send_file("index.html")
+    if request.method == 'POST':
+        content = request.json
+        
+app.debug = True
 app.run(host="0.0.0.0", port=5000)
