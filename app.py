38e1595fcc1c60f59b4df6c8b9e7805661eff48f
Samuel Damashek <2017sdamashe@minecraft.csl.tjhsst.edu>
2013-10-12 12:34:30 -0400

    More work on auth

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask, request, render_template, send_file
 import json
+import sqlite3
+import uuid
 app = Flask(__name__)
+conn = sqlite3.connect('auth.db', check_same_thread = False)
+c = conn.cursor()
 
 ################## Errors ##################
 def method_not_allowed():
     args = {"error":"Method Not Allowed","errorMessage":"The method specified in the request is not allowed for the resource identified by the request URI"}
-    return json.dumps(args)
+    return json.dumps(args), 405
 
 def invalid_credentials_migrated():
     args = {"error":"ForbiddenOperationException","errorMessage":"Invalid credentials. Account migrated, use e-mail as username.","cause":"UserMigratedException"}
-    return json.dumps(args)
+    return json.dumps(args), 403
 
 def invalid_credentials():
     args = {"error":"ForbiddenOperationException","errorMessage":"Invalid credentials. Invalid username or password."}
-    return json.dumps(args)
+    return json.dumps(args), 403
 
 def invalid_token():
     args = {"error":"ForbiddenOperationException","errorMessage":"Invalid token."}
-    return json.dumps(args)
+    return json.dumps(args), 403
 
 def illegal_argument_exception():
     args = {"error":"IllegalArgumentException","errorMessage":"Access token already has a profile assigned."}
@@ -25,8 +29,14 @@ def illegal_argument_exception():
 
 def unsupported_media_type():
     args = {"error":"Unsupported Media Type","errorMessage":"The server is refusing to service the request because the entity of the request is in a format not supported by the requested resource for the requested method"}
-    return json.dumps(args)
+    return json.dumps(args), 415
 
+def custom_exception(args):
+    return json.dumps(args), 400
+    
+################## Authentication Methods ##
+def validate_user(username, password):
+    return True
 ################## Endpoints ###############
 
 @app.route('/', methods=['GET', 'POST'])
@@ -41,12 +51,31 @@ def authenticate():
         return method_not_allowed()
     if request.mimetype != 'application/json':
         return unsupported_media_type()
-    json = request.get_json()
+    try:
+        content = request.get_json()
+        if 'username' not in content or 'password' not in content:
+            return invalid_credentials()
+        if 'clientToken' not in content:
+            content['clientToken'] = str(uuid.uuid4())
+        if not validate_user(content['username'],content['password']):
+            return invalid_credentials()
+        c.execute("DELETE FROM users WHERE username=?", content['username'])
+        accessToken = str(uuid.uuid4())
+        c.execute("INSERT INTO users (username, clientToken, accessToken) VALUES (?,?,?)", content['username'],content['clientToken'],accessToken)
+        conn.commit()
+        response = {"accessToken":accessToken,"clientToken":content['clientToken']}
+        return json.dumps(response)
+
+
+
+    except Exception as e:
+        return custom_exception({"error":str(type(e).__name__), "errorMessage":str(e)})
+
 
 @app.errorhandler(404)
 def errornotfound(e):
     args = {"error":"Not Found","errorMessage":"The server has not found anything matching the request URI"}
-    return json.dumps(args)
+    return json.dumps(args), 404
 
 app.debug = True
 app.run(host="0.0.0.0", port=5000)
