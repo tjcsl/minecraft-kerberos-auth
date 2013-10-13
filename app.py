b626eadd474006fe01d0032b307daf447dce161f
Samuel Damashek <2017sdamashe@minecraft.csl.tjhsst.edu>
2013-10-13 00:03:04 -0400

    Return selectedProfile on refresh

--- app.py
+++ app.py
@@ -112,9 +112,10 @@ def refresh():
         client = u[3]
         userid = u[0] 
         access = str(uuid.uuid4())
+        profileId = uuid.uuid4().hex
         c.execute("UPDATE users SET accessToken='%s' WHERE ID=?" % access, (userid,))
         conn.commit()
-        response = {"accessToken":access,"clientToken":client}
+        response = {"accessToken":access,"clientToken":client,"selectedProfile":{"id":profileId,"name":content['username']}}
         return json.dumps(response)
     except Exception as e:
         return custom_exception({"error":str(type(e).__name__), "errorMessage":str(e)})
